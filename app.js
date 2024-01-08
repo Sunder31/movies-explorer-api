4af78274437581ff06ef34c05fea42353add56e3
Andrey <101015074+Sunder31@users.noreply.github.com>
2024-01-08 22:35:57 +0300

    Update app.js2

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const app = express();
 
 app.use(cors({
    credentials: true,
-   origin: ['https://sunder.movie-explorer.nomoredomainsmonster.ru', 'http://localhost:3000', 'http://localhost:5173'],
+   origin: ['https://sunder.movie-explorer.nomoredomainsmonster.ru', 'http://localhost:3000', 'http://localhost:5173', 'http://127.0.0.1:3000'],
 }));
 
 app.use(helmet());
